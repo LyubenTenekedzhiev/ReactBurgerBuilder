99f1afa92b8340e7d914f54f3da81b1bcf5a8d7a
Lyuben Tenekedzhiev <lyuben.tenekedjiev@gmail.com>
2020-02-19 22:02:23 +0100

    with routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends React.Component {
 
@@ -9,7 +12,11 @@ class App extends React.Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/orders' component={Orders} />
+            <Route path='/' exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,9 +5,10 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
+
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -30,13 +31,13 @@ class BurgerBuilder extends React.Component {
     axios.get('https://burgerbuildertwo.firebaseio.com/ingredients.json')
             .then(response => {
               this.setState({ingredients: response.data})
+              console.log(this.state.ingredients);
             }).catch(error => {
               this.setState({error: true})
             })
   }
 
   updatePurchaseState = (ingredients) => {
-   
     const sum = Object.keys(ingredients).map(igKey => {
       return ingredients[igKey];
     }).reduce((sum, el) => {
@@ -85,29 +86,16 @@ class BurgerBuilder extends React.Component {
   }
 
   purchaseContinueHandler = () => {
-    //Showing Spinner
-    this.setState({loading: true});
-    
-    //Posting data to the Firebase
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Lyuben Tenekedzhiev',
-        adress: {
-          street: 'PRAGUE BABE',
-          country: 'CZECH REP',
-        },
-        email: 'lyuben@gmail.com'
-      },
-      deliveryMethod: 'fastest',
+    const queryParams = [];
+    for(let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    axios.post('/orders.json', order)
-          .then(response => {
-            this.setState({ loading: false, purchasing: false })
-          }).catch(error => {
-            this.setState({ loading: false, purchasing: false })
-          });
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    })
   }
 
   render() {
